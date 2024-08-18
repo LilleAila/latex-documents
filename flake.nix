72cf18cf7721c2acbf0b82c9293949e8b7cd626d
LilleAila <olai.solsvik@gmail.com>
2024-08-18 18:16:39 +0200

    Added devshell and initial preamble

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "LaTeX flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # autotex.url = "github:e0328eric/autotex";
+    # autotex.flake = false;
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      lib = nixpkgs.lib;
+      systems = lib.systems.flakeExposed;
+      pkgsFor = lib.genAttrs systems (system: import nixpkgs { inherit system; });
+      forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+    in
+    {
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nixd
+            nixfmt-rfc-style
+            # TODO: only specify used packages, as described in https://nixos.wiki/wiki/TexLive
+            texlive.combined.scheme-full
+            pandoc
+            zathura
+            (python311.withPackages (ps: with ps; [ pygments ]))
+            # (rustPlatform.buildRustPackage {
+            #   pname = "autotex";
+            #   version = "master";
+            #   src = inputs.autotex;
+            #   cargoHash = "sha256-AlKYlNALAW0R6vlGlNlUQUkKvcxYJ+8WCMhU6weLEWc=";
+            # })
+            (writeShellScriptBin "latex-compile" ''latexmk -pdf -lualatex -lualatex="lualatex %O --shell-escape %S" -pvc'')
+          ];
+        };
+      });
+      # TODO: maybe provide compiled documents as derivations?
+    };
+}
